fd15d60972fc794868e6a5396b8990675f286566
RemKuzo <rikuzo36@gmail.com>
2020-09-04 03:08:50 +0200

    chore: connected sign up form to the firebase

--- Scripts/signUp.js
+++ Scripts/signUp.js
@@ -1,46 +1,90 @@
-const name = document.getElementById('name');
-const password = document.getElementById('password');
-const form = document.getElementById('form');
-const confirmPassword = document.getElementById('confirmPassword');
-const email = document.getElementById('email');
 const error = document.getElementById('error');
+let success = document.getElementById('success');
+const confirmPassword = document.getElementById('confirmPassword');
+const submitbtn = document.getElementById('submitbtn');
+let info = [];
+let data = [];
 
-form.addEventListener('submit', (e) => {
-    let message = [];
-    if(!isName((name.value))|| (name.value.length <= 3)) {
-        message.push('Name is invalid');
-        document.getElementById('span1').style.display= 'inline-block';
-     }
-     if (!isEmail(email.value)) {
-        message.push('Email is invalid')
-        document.getElementById('span2').style.display= 'inline-block';
-        
-        
-    }
-     if (!isPassword(password.value)) {
-        message.push('Password must contain atleast one capital letter and a number !');
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyA0c0NclmrcZErMkWshnxHiPGc7NJZrkO8",
+    authDomain: "capstone-project-remy.firebaseapp.com",
+    databaseURL: "https://capstone-project-remy.firebaseio.com",
+    projectId: "capstone-project-remy",
+    storageBucket: "capstone-project-remy.appspot.com",
+    messagingSenderId: "610521963750",
+    appId: "1:610521963750:web:4fcd0d1c3cc7b3b867af27"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var db=firebase.firestore();
+  
+  var form=document.getElementById('form');
+  form.addEventListener('submit',send);
+
+
+  
+
+  function send(e){
+      e.preventDefault();
+
+      var name=getInput('name');
+      var email=getInput('email');
+      var password=getInput('password');
+
+     if(!isName((name))|| (name.length <= 3)) {
+       info.push('Name is invalid');
+      
+      }
+     if (!isEmail(email)) {
+      info.push('Email is invalid');
+       
+      }
+      if (!isPassword(password)) {
+        info.push('Password must contain atleast one capital letter and a number !');
          document.getElementById('span3').style.display= 'inline-block';
      
     }
-     if (password.value !== confirmPassword.value) {
+    /* if (password !== confirmPassword.value) {
          document.getElementById('span4').style.display= 'inline-block';
-         message.push('Password doesn\'t match');
+         info.push('Password doesn\'t match');
         
+    }*/
+     if (info.length > 0) {
+      e.preventDefault();
+     error.innerText = info.join('\n');
+     return false;
     }
-    if (message.length > 0) {
-        e.preventDefault();
-        error.innerText = message.join('\n');
+
+   
+
+      db.collection('signUp').add({
+          name:name,
+          email:email,
+          password:password
+
+      })
+      
+      document.getElementById('form').reset();
+      data.push('Data has been sent');
+      success.innerText = data.join('\n');
+
+      }
+
+    function getInput(id){
+    return document.getElementById(id).value;
     }
-    
-});
-function isName(name){
-    return /^[a-zA-Z' ']+$/.test(name)
-}
-function isEmail(email) {
-    return /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/.test(email)
-}
-function isPassword(password) {
-    return /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/.test(password)
-}
+    function isName(name){
+      return /^[a-zA-Z' ']+$/.test(name)
+    }
+    function isEmail(email) {
+      return /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/.test(email)
+    }
+    function isPassword(password) {
+        return /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,}/.test(password)
+    }
+   
 
 
